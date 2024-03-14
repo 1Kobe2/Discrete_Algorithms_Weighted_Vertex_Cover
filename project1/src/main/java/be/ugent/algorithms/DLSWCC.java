48d2578bfc671832c4449d032a4b5f9751021682
1Kobe2 <kobebleuze@live.be>
2024-03-14 17:09:37 +0100

    DLSWCC initial commit

---
+++ project1/src/main/java/be/ugent/algorithms/DLSWCC.java
@@ -0,0 +1,14 @@
+package be.ugent.algorithms;
+
+import be.ugent.benchmark.IntermediateSolutionReporter;
+import be.ugent.graphs.BasicGraph;
+
+import java.util.BitSet;
+
+public class DLSWCC implements WeightedVertexCoverAlgorithm {
+
+    @Override
+    public BitSet calculateMinVertexCover(BasicGraph graph, IntermediateSolutionReporter intermediateSolutionReporter) {
+        return null;
+    }
+}
